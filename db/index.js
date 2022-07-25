83ceb870e515b91aefd67f2afd6f78705b31e7a8
Andy Duff <andyduffdj25@gmail.com>
2022-07-25 19:13:38 -0300

    deploy heroku

--- db/index.js
+++ db/index.js
@@ -1,14 +1,19 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize(
-    process.env.PGDATABASE,
-    process.env.PGUSER,
-    process.env.PGPASSWORD,
-    {
-        host: process.env.PGHOST,
-        dialect: "postgres"
-    }
-)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    database: process.env.PGDATABASE,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+  });
 
 module.exports = {
     sequelize
